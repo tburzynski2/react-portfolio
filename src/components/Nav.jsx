e841458d3b24b61c5801456a79ebb95602db8eae
Tom Bee <tburzynski2@yahoo.com>
2024-08-14 15:11:28 -0500

    Improved Nav component link styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,23 +1,34 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-const Nav = ({ activePage, setActivePage }) => {
-  const getActiveClass = ({ isActive }) => (isActive ? "active" : "");
+const Nav = () => {
+  const getActiveClass = ({ isActive }) =>
+    isActive ? "nav-link active" : "nav-link";
 
   return (
     <nav>
-      <NavLink to="/about" className={getActiveClass}>
-        About Me
-      </NavLink>
-      <NavLink to="/portfolio" className={getActiveClass}>
-        Portfolio
-      </NavLink>
-      <NavLink to="/contact" className={getActiveClass}>
-        Contact
-      </NavLink>
-      <NavLink to="/resume" className={getActiveClass}>
-        Resume
-      </NavLink>
+      <ul className="nav">
+        <li className="nav-item">
+          <NavLink to="/about" className={getActiveClass}>
+            About Me
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/portfolio" className={getActiveClass}>
+            Portfolio
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/contact" className={getActiveClass}>
+            Contact
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/resume" className={getActiveClass}>
+            Resume
+          </NavLink>
+        </li>
+      </ul>
     </nav>
   );
 };
